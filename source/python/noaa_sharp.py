fb5d334c09e7ff967faebea39dab66d7309d08b8
David MacTaggart <98175493+DavidMacT@users.noreply.github.com>
2022-12-20 15:47:34 +0100

    Update noaa_sharp.py

--- source/python/noaa_sharp.py
+++ source/python/noaa_sharp.py
@@ -1,9 +1,26 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 
 from __future__ import division, print_function
 import os.path
 import pandas as pd
+import requests
+import os
+import webbrowser
+
+
+def NOAA_HARP_number(year, month, day,hour):
+    Date = "{:02d}".format(year)+'/'+"{:02d}".format(month)+'/'+"{:02d}".format(day)
+    DateTime = "{:02d}".format(year)+'.'+"{:02d}".format(month)+'.'+"{:02d}".format(day)+'_'+ "{:02d}".format(hour)        
+    image = "http://jsoc.stanford.edu/doc/data/hmi/harp/harp_definitive/"+Date+"/harp."+DateTime+":00:00_TAI.png"
+    response = requests.get(image)
+    
+    if response.status_code == 200:
+        webbrowser.open(image)
+    else:
+        image = "http://jsoc.stanford.edu/doc/data/hmi/harp/harp_nrt/harp."+DateTime+":00:00_TAI.png"
+        webbrowser.open(image)
 
 
 def getSHARP_number(NOAA_ARegion):
@@ -16,16 +33,24 @@ def getSHARP_number(NOAA_ARegion):
         data['SHARP'].append(AR[0])
         data['NOAA_ARS'].append(AR[1].split(','))
 
-
     sharp_number=[]
-
     for R in NOAA_ARegion:
         for i,S in enumerate(data['NOAA_ARS']):
             if str(R) in S:
                 sharp_number.append(data['SHARP'][i])
-    return sharp_number
-
-            
-
-x = getSHARP_number([12673, 11318])  # choose NOAA region numbers
-print(x)
+    if len(sharp_number) == 0:
+        print('More details are needed:')
+        year = input('insert the year: ')
+        month = input('insert the month: ')
+        day = input('insert the day: ')
+        hour = input('insert the hour: ')
+        NOAA_HARP_number(int(year),int(month), int(day),int(hour))
+    else:
+        print(sharp_number)
+        return sharp_number
+          
+"""
+Get the SHARP number
+"""
+Noaa = int(input('insert NOAA number: '))
+getSHARP_number([Noaa])  # Insert NOAA region numbers
